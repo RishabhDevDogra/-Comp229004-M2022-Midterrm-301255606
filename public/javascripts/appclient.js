2a1ce57b144e3cb3a911757e7e906188d00ddf6e
RishabhDevDogra <105988215+RishabhDevDogra@users.noreply.github.com>
2022-06-25 21:05:13 -0400

    Update appclient.js

--- public/javascripts/appclient.js
+++ public/javascripts/appclient.js
@@ -1,7 +1,7 @@
 // Filename: appclient.js
-// Student: Julio Vinicius
-// StudentId: 123456789
-// Date: Jun 02, 2022
+// Student: Rishabh Dev Dogra
+// StudentId: 301255606
+// Date: Jun 25, 2022
 
 console.log('app script is working.');
 
